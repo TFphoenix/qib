90a5f63e81837026a0f5ca0e4858d8a0bf6ede6c
Teodor <teodor@mihaescu.info>
2024-01-10 02:13:25 +0200

    TEST: QiskitSimulator test WIP

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -7,7 +7,7 @@ class TestBackend(unittest.TestCase):
 
     def test_tensor_network(self):
         """
-        Test tensor network backend functionality.
+        Test tensor network processor functionality.
         """
         field = qib.field.Field(
             qib.field.ParticleType.QUBIT, qib.lattice.IntegerLattice((2,)))
@@ -25,9 +25,34 @@ class TestBackend(unittest.TestCase):
         h_cnot = cnot.as_matrix() @ np.kron(hadamard.as_matrix(), np.identity(2))
         self.assertTrue(np.array_equal(
             circuit.as_matrix([field]).toarray(), h_cnot))
-        provider = qib.backend.TensorNetworkProcessor()
-        provider.submit(circuit, [field], {
-                        "filename": "bell_circuit_tensornet.hdf5"})
+        processor = qib.backend.TensorNetworkProcessor()
+        processor.submit(circuit, [field], {
+            "filename": "bell_circuit_tensornet.hdf5"})
+
+    def test_qiskit_sim(self):
+        """
+        Test Qiskit Simulator processor functionality.
+        """
+        # Qubits
+        field = qib.field.Field(
+            qib.field.ParticleType.QUBIT, qib.lattice.IntegerLattice((2,)))
+        qa = qib.field.Qubit(field, 0)
+        qb = qib.field.Qubit(field, 1)
+
+        # Gates
+        hadamard = qib.HadamardGate(qa)
+        cnot = qib.ControlledGate(qib.PauliXGate(qb), 1).set_control(qa)
+
+        # Circuit
+        circuit = qib.Circuit()
+        circuit.append_gate(hadamard)
+        circuit.append_gate(cnot)
+        # Measurement gate ???
+
+        # Processor
+        processor = qib.backend.QiskitSimProcessor()
+        processor.submit(circuit, [field], {
+            "filename": "bell_circuit_qiskit_sim.hdf5"})
 
 
 if __name__ == "__main__":
