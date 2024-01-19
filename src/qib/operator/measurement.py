44a2727f436fc808fcef9b5fac904834ea3f6ddb
Teodor <teodor@mihaescu.info>
2024-01-19 13:28:33 +0100

    FEAT: Implement measurement operator

--- src/qib/operator/measurement.py
+++ src/qib/operator/measurement.py
@@ -1,73 +1,69 @@
 import abc
+from typing import Sequence
 from qib.operator import AbstractOperator
+from qib.field import Field, Particle, Qubit
 
 
 class Measurement(AbstractOperator):
     """
-    Measurement operation for a quantum circuit.
+    Measurement operator for a quantum circuit.
 
-    TODO
+    A quantum measurement represents the corresponding operator that measures a quantum bit into a classical bit,
+    and can store the qubits (or in general quantum particles) it operates on.
     """
+
+    def __init__(self, qubits: Sequence[Qubit] = None):
+        self.qubits = qubits
 
     def is_unitary(self):
         """
-        A quantum measurement operation is never unitary.
+        A quantum measurement operator is never unitary.
+        """
+        return False
+
+    def is_hermitian(self):
+        """
+        A quantum measurement operator is always non-Hermitian.
         """
         return False
 
     @property
-    @abc.abstractmethod
     def num_wires(self):
         """
-        The number of "wires" (or quantum particles) this operation is performed on.
+        The number of "wires" (or quantum particles) this operator is performed on.
         """
+        if self.qubits:
+            return len(self.qubits)
+        return 0
 
-    @abc.abstractmethod
     def particles(self):
         """
-        Return the list of quantum particles the operation is performed on.
+        Return the list of quantum particles the operator is performed on.
         """
+        if self.qubits:
+            return [q for q in self.qubits]
+        return []
 
-    @abc.abstractmethod
     def fields(self):
         """
-        Return the list of fields hosting the quantum particles which the operation is performed on.
+        Return the list of fields hosting the quantum particles which the operator is performed on.
         """
+        if self.qubits:
+            return [q.field for q in self.qubits]
+        return []
 
-    @abc.abstractmethod
-    def inverse(self):
+    def as_matrix(self, fields: Sequence[Field]):
         """
-        Return the inverse operator.
+        Generate the (sparse) matrix representation of the operator.
         """
+        raise NotImplementedError()  # TODO: Decide if this is needed
 
-    @abc.abstractmethod
-    def as_circuit_matrix(self, fields: Sequence[Field]):
-        """
-        Generate the sparse matrix representation of the gate
-        as element of a quantum circuit.
-        """
-
-    @abc.abstractmethod
-    def as_tensornet(self):
-        """
-        Generate a tensor network representation of the gate,
-        using an individual tensor axis for each wire.
-        """
-        
-    @abc.abstractmethod
     def as_qobj_openQASM(self):
         """
-        Generate a Qobj OpenQASM representation of the gate.
+        Generate a Qobj OpenQASM representation of the operator.
         """
-
-    @abc.abstractmethod
-    def __copy__(self):
-        """
-        Create a copy of the gate.
-        """
-
-    @abc.abstractmethod
-    def __eq__(self, other):
-        """
-        Check if gates are equivalent.
-        """
+        return {
+            "name": "measure",
+            "qubits": [q.index for q in self.qubits],
+            "memory": [q.index for q in self.qubits]
+        }
---
+++ tests/test_measurement.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestMeasurement(unittest.TestCase):
+    def test_measurement(self):
+        pass  # TODO
